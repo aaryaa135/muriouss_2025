27b79b677c9e7837001456a99acfcf9b0c462707
dwag-cyber <tg1570@gmail.com>
2025-01-11 00:48:25 +0530

    parts: made like murious

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,22 +39,22 @@ import {
 export const navigation = [
   {
     id: "0",
-    title: "Features",
+    title: "About",
     url: "#features",
   },
   {
     id: "1",
-    title: "Pricing",
+    title: "Events",
     url: "#pricing",
   },
   {
     id: "2",
-    title: "How to use",
+    title: "Sponsors",
     url: "#how-to-use",
   },
   {
     id: "3",
-    title: "Roadmap",
+    title: "Contact",
     url: "#roadmap",
   },
   {
